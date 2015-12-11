d4b861a3100c1298a753b966415d3c94691f0a7e
Shana L Moore <shana.lavina.moore@gmail.com>
2015-12-10 20:42:41 -0800

    first commit

---
+++ get_number.rb
@@ -0,0 +1,15 @@
+#Get My Number Game
+#Written by: Shana Moore
+
+puts "Welcome to 'Get My Number!'"
+
+#Gets the player's name, and greet them.
+print "What's your name? "
+input = gets
+name = input.chomp
+puts "Welcome, #{name}!"
+
+#Store a random number for the player to guess. 
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1 
